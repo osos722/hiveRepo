307b592bb88eee9924678aca7089c061890be0c1
osama emad <112451300+osos722@users.noreply.github.com>
2025-05-20 12:02:07 +0300

    create notes view and change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hivetutorial/views/NotesView.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
@@ -27,6 +27,11 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  MaterialApp(
+      theme:ThemeData(
+        brightness: Brightness.dark
+      ) ,
+      home: const NotesView(),
+    );
   }
 }
