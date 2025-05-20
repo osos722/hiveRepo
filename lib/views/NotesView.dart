81edacd19424a4a4faa739d830a2a2d947257e60
osama emad <112451300+osos722@users.noreply.github.com>
2025-05-20 16:01:59 +0300

    create Buttom Sheet

--- lib/views/NotesView.dart
+++ lib/views/NotesView.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:hivetutorial/widgets/ViewBody.dart';
+import 'package:hivetutorial/widgets/addnoteButtomSheet.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16)
+        ),
+        onPressed: (){
+        showModalBottomSheet(context: context, builder: (cotext){
+          return const Addnotebuttomsheet();
+        });
+      },child:const  Icon(Icons.add),),
+      body:const NotesViewBody(),
     );
   }
 }
--- lib/widgets/notesListView.dart
+++ lib/widgets/notesListView.dart
@@ -6,13 +6,16 @@ class Noteslistview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  ListView.builder(
-      itemBuilder: (context,index){
-        return const Padding(
-          padding:  EdgeInsets.symmetric(vertical: 8),
-          child:  NoteItem(),
-        );
-      }
-      );
+    return  Padding(
+      padding: const EdgeInsets.symmetric(vertical:16),
+      child: ListView.builder(
+        itemBuilder: (context,index){
+          return const Padding(
+            padding:  EdgeInsets.symmetric(vertical: 8),
+            child:  NoteItem(),
+          );
+        }
+        ),
+    );
   }
 }
