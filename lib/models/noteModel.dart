f5e67c883f03026caca0aaacdbf9eb916744b3ab
osama emad <112451300+osos722@users.noreply.github.com>
2025-05-22 06:13:11 +0300

    create note model

---
+++ lib/models/noteModel.dart
@@ -0,0 +1,12 @@
+class NoteModel{
+  final String noteTitle;
+  final String noteBody;
+  final String date;
+  final int color;
+  NoteModel({
+    required this.color,
+    required this.date,
+    required this.noteBody,
+    required this.noteTitle
+  });
+}
--- lib/widgets/editNoteBody.dart
+++ lib/widgets/editNoteBody.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hivetutorial/widgets/customApppBar.dart';
+import 'package:hivetutorial/widgets/customTextField.dart';
 
 class EditnoteBody extends StatelessWidget {
   const EditnoteBody({super.key});
@@ -9,7 +10,11 @@ class EditnoteBody extends StatelessWidget {
     return const Padding(
       padding:  EdgeInsets.symmetric(horizontal: 24),
       child: Column(children: [
-        CustomAppBar(text: 'Edit', icona: Icons.check,)
+        CustomAppBar(text: 'Edit', icona: Icons.check,),
+        SizedBox(height: 50,),
+        CustomTextField(hintText: 'title'),
+        SizedBox(height: 16,),
+        CustomTextField(hintText: 'body',maxLines: 5,),
       ],),
     );
   }
